cb84164eaf1a02f9052f5ccc6d6b77d2d77b764d
LiantsoaANDR <liantsoa.cpro@gmail.com>
2023-10-19 23:55:09 -0700

    trying something

--- ex_cmd.c
+++ ex_cmd.c
@@ -39,6 +39,12 @@ void executer(char *cmd, char **array)
 	pid_t child_process;
 	int status;
 
+	if (!cmd)
+	{
+		perror(array[0]);
+		free_array(array);
+		return;
+	}
 	child_process = fork();
 	if (child_process == -1)
 	{
